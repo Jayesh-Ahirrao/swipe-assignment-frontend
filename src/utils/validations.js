ba8fa8455e893fb0676a78bfe2d2466f8f0cf37d
Jayesh <en001507@gmail.com>
2024-08-02 20:24:30 +0530

    Feat: Making code modular

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,191 +1,89 @@
 import { CATEGORIES } from "../constants/categories";
 var validator = require("email-validator");
 
-export const validateProduct = (product) => {
-    const res = {
-        passed: false,
-        message: "",
+class ValidationResponse {
+    constructor(message, passed = false) {
+        this.passed = passed;
+        this.message = message;
     }
-
-    if (!product) {
-        res.message = "Invalid product";
-        return res;
-    }
-
-    if (!product.name || product.name.trim() === '') {
-        res.message = "Please Enter a name of the product";
-        return res;
-    }
-
-    if (!product.description || product.description.trim() === '') {
-        res.message = "Please Enter a name of the product";
-        return res;
-    } else if (product.description.trim().length < 4) {
-        res.message = "Please provide clear description of the product";
-        return res;
-    }
-
-    if (!product.price || product.price < 0.1) {
-        res.message = "Price of the product must be between greater than 0.1";
-        return res;
-    }
-
-    res.passed = true;
-    return res;
 }
 
+export const validateProduct = (product) => {
+    if (!product) return new ValidationResponse("Invalid product");
 
-// const item = {
-//     itemId: id,
-//     itemName: "",
-//     itemDescription: "",
-//     itemPrice: "1.00",
-//     itemQuantity: 1,
-//     category: "",
-// };
+    if (!product.name || product.name.trim() === '') return new ValidationResponse("Enter a name of the product");
 
+    if (!product.description || product.description.trim() === '') return new ValidationResponse("Enter a description of the product")
+
+    else if (product.description.trim().length < 4) return new ValidationResponse("Please provide clear description of the product");
+
+    if (!product.price || product.price < 0.1) return new ValidationResponse("Price of the product must be between greater than 0.1");
+
+    return new ValidationResponse("Product added successfully", true);
+}
 
 /**
  * validation for single item
  * @returns 
  */
 export const validateItem = (item) => {
-    const res = {
-        passed: false,
-        message: "",
-    }
+    if (!item) return new ValidationResponse("Invalid item");
 
+    if (item.itemName.length < 3) return new ValidationResponse("Provide valid name for an Product");
 
-    if (!item) {
-        res.message = "Invalid Product";
-        return res;
-    }
+    if (!item.itemDescription || item.itemDescription.length < 3) return new ValidationResponse("Provide valid description for an Product");
 
-    if (item.itemName.length < 3) {
-        res.message = "Provide valid name for an Product";
-        return res;
-    }
+    if (item.itemPrice < 0.1) return new ValidationResponse("Provide a price for an Product greater than 0.1");
 
-    if (!item.itemDescription || item.itemDescription.length < 3) {
-        res.message = "Provide valid description for an Product";
-        return res;
-    };
+    if (item.itemQuantity < 1) return new ValidationResponse("Provide quantity for Product");
 
-    if (item.itemPrice < 0.1) {
-        res.message = "Provide a price for an Product greater than 0.1";
-        return res;
-    }
-    if (item.itemQuantity < 1) {
-        res.message = "Provide quantity for Product";
-        return res;
-    }
+    if (![CATEGORIES.GOODS, CATEGORIES.SERVICES].includes(item.category)) return new ValidationResponse("Select a category of  Product");
 
-    if (![CATEGORIES.GOODS ,CATEGORIES.SERVICES].includes(item.category) ) {
-        res.message = "Select a category of  Product";
-        return res;
-    }
-
-    res.passed = true;
-    return res;
+    return new ValidationResponse("Successfull", true);
 }
 
 
-export const validateInvoice = ( invoice , invoiceList =[]) => {
-    const res = {
-        passed: false,
-        message: "",
-    }
+export const validateInvoice = (invoice, invoiceList = []) => {
+    if (!invoice) return new ValidationResponse("This invoice number is already in use");
 
-    if(!invoice) {
-        res.message = "Invalid invoice";
-        return res;
-    }
     // unique invoice number
-    const isIdNotUnique = invoiceList.find((record) => record.invoiceNumber === invoice.invoiceNumber )
-
-    if(isIdNotUnique) {
-        res.message = "This invoice number is already in use";
-        return res;
-    }
+    const isIdNotUnique = invoiceList.find((record) => record.invoiceNumber === invoice.invoiceNumber)
+    if (isIdNotUnique) return new ValidationResponse("This invoice number is already in use");
 
     // dateOfIssue
-    if(!invoice.dateOfIssue) {
-        res.message = "Select a Due date";
-        return res;
-    }
+    if (!invoice.dateOfIssue) return new ValidationResponse("Select a Due date");
 
     // addresses and emails
-    if(!invoice.billTo || !invoice.billTo.trim()) {
-        res.message = "Enter clients name";
-        return res;
-    }
+    if (!invoice.billTo || !invoice.billTo.trim()) return new ValidationResponse( "Enter clients name");
 
-    if(!invoice.billToEmail || !validator.validate(invoice.billToEmail) ) {
-        res.message = "Enter clients valid email address";
-        return res;
-    }
+    if (!invoice.billToEmail || !validator.validate(invoice.billToEmail)) return new ValidationResponse("Enter clients valid email address");
 
-    if(!invoice.billToAddress || !invoice.billToAddress.trim() ) {
-        res.message = "Provide client address";
-        return res;
-    }
+    if (!invoice.billToAddress || !invoice.billToAddress.trim()) return new ValidationResponse("Provide client address");
 
     // owner
-    if(!invoice.billFrom || !invoice.billFrom.trim()) {
-        res.message = "Enter providers name";
-        return res;
-    }
+    if (!invoice.billFrom || !invoice.billFrom.trim()) return new ValidationResponse("Enter providers name");
 
-    if(!invoice.billFromEmail || !validator.validate(invoice.billFromEmail) ) {
-        res.message = "Enter providers valid email address";
-        return res;
-    }
+    if (!invoice.billFromEmail || !validator.validate(invoice.billFromEmail)) return new ValidationResponse("Enter providers valid email address");
 
-    if(!invoice.billFromAddress || !invoice.billFromAddress.trim() ) {
-        res.message = "Provide providers address";
-        return res;
-    }
+    if (!invoice.billFromAddress || !invoice.billFromAddress.trim()) return new ValidationResponse("Provide providers address");
 
     // items
-    if(!invoice.items || invoice.items.length === 0 ){
-        res.message = "Add Product to invoice";
-        return res;
-    }
+    if (!invoice.items || invoice.items.length === 0) return new ValidationResponse("Add Product to invoice");
 
+    // to check individaul items
     let result;
-
     let isItemInvalid = invoice.items.some((item) => {
         result = validateItem(item);
         return !result.passed;
     });
+    if (isItemInvalid) return result;
 
-    if(isItemInvalid) return result; 
-
-    // prices subtotal tax discount rate amount
     // doing this because the tax ratecan be "" for value of '0.0' 
-    if(invoice.taxRate && invoice.taxRate < 0 ) {
-        res.message = "Invalid tax rate";
-        return res;
-    }
+    if (invoice.taxRate && invoice.taxRate < 0) return new ValidationResponse("Invalid tax rate");
 
-    if(invoice.discountRate && invoice.discountRate < 0 ) {
-        res.message = "Invalid Discount rate";
-        return res;
-    }
+    if (invoice.discountRate && invoice.discountRate < 0) return new ValidationResponse("Invalid Discount rate");
 
-    if(invoice.subTotal < 0.1) {
-        res.message = "Total cannot be negative";
-        return res;
-    }
+    if (!invoice.currency) return new ValidationResponse("Select Valid currency");
 
-    // currency
-    if(!invoice.currency) {
-        res.message = "Select Valid currency";
-        return res;
-    }   
-
-    res.passed = true;
-    return res; 
+    return new ValidationResponse("Invoice Created", true);
 };
-
-
