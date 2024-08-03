270a40598156475f96e821f5d7387980be51fa0a
Jayesh <en001507@gmail.com>
2024-08-03 08:50:15 +0530

    Trying netlify redirects

--- src/pages/InvoiceList.jsx
+++ src/pages/InvoiceList.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { Button, Card, Col, Row, Table } from "react-bootstrap";
 import { BiSolidPencil, BiTrash } from "react-icons/bi";
 import { BsEyeFill } from "react-icons/bs";
 import InvoiceModal from "../components/InvoiceModal";
 import { useNavigate } from "react-router-dom";
-import { useInvoiceListData, useProductListData } from "../redux/hooks";
+import { useInvoiceListData } from "../redux/hooks";
 import { useDispatch } from "react-redux";
 import { deleteInvoice } from "../redux/invoicesSlice";
 import GoToButton from "../UI/GoToButton";
