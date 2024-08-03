3e61dbbf4d44b4684e63b19e37326dd58c43d99d
Jayesh <en001507@gmail.com>
2024-08-03 08:59:25 +0530

    Updating products

--- src/data/products.js
+++ src/data/products.js
@@ -8,67 +8,67 @@ export const dummyProducts = [
     price: 1499.99,
     category: CATEGORIES.GOODS
   },
-  // {
-  //   id: 2,
-  //   name: "Room Cleaning Service",
-  //   description: "Thorough room cleaning.",
-  //   price: 29.99,
-  //   category: CATEGORIES.SERVICES
-  // },
-  // {
-  //   id: 3,
-  //   name: "Gourmet Breakfast",
-  //   description: "Deluxe breakfast spread.",
-  //   price: 19.99,
-  //   category: CATEGORIES.SERVICES
-  // },
-  // {
-  //   id: 4,
-  //   name: "Premium Towels",
-  //   description: "Soft and high-quality towels.",
-  //   price: 79.99,
-  //   category: CATEGORIES.GOODS
-  // },
-  // {
-  //   id: 5,
-  //   name: "Spa Treatment",
-  //   description: "Relaxing spa experience.",
-  //   price: 129.99,
-  //   category: CATEGORIES.SERVICES
-  // },
-  // {
-  //   id: 6,
-  //   name: "Mini Bar",
-  //   description: "Stocked mini bar with snacks.",
-  //   price: 59.99,
-  //   category: CATEGORIES.GOODS
-  // },
-  // {
-  //   id: 7,
-  //   name: "Laundry Service",
-  //   description: "Professional laundry care.",
-  //   price: 15.99,
-  //   category: CATEGORIES.SERVICES
-  // },
-  // {
-  //   id: 8,
-  //   name: "Fitness Center Access",
-  //   description: "Access to gym and equipment.",
-  //   price: 12.99,
-  //   category: CATEGORIES.SERVICES
-  // },
-  // {
-  //   id: 9,
-  //   name: "Bathrobe",
-  //   description: "Plush and soft bathrobe.",
-  //   price: 99.99,
-  //   category: CATEGORIES.GOODS
-  // },
-  // {
-  //   id: 10,
-  //   name: "Coffee Maker",
-  //   description: "Coffee maker for fresh brews.",
-  //   price: 199.99,
-  //   category: CATEGORIES.GOODS
-  // }
+  {
+    id: 2,
+    name: "Room Cleaning Service",
+    description: "Thorough room cleaning.",
+    price: 29.99,
+    category: CATEGORIES.SERVICES
+  },
+  {
+    id: 3,
+    name: "Gourmet Breakfast",
+    description: "Deluxe breakfast spread.",
+    price: 19.99,
+    category: CATEGORIES.SERVICES
+  },
+  {
+    id: 4,
+    name: "Premium Towels",
+    description: "Soft and high-quality towels.",
+    price: 79.99,
+    category: CATEGORIES.GOODS
+  },
+  {
+    id: 5,
+    name: "Spa Treatment",
+    description: "Relaxing spa experience.",
+    price: 129.99,
+    category: CATEGORIES.SERVICES
+  },
+  {
+    id: 6,
+    name: "Mini Bar",
+    description: "Stocked mini bar with snacks.",
+    price: 59.99,
+    category: CATEGORIES.GOODS
+  },
+  {
+    id: 7,
+    name: "Laundry Service",
+    description: "Professional laundry care.",
+    price: 15.99,
+    category: CATEGORIES.SERVICES
+  },
+  {
+    id: 8,
+    name: "Fitness Center Access",
+    description: "Access to gym and equipment.",
+    price: 12.99,
+    category: CATEGORIES.SERVICES
+  },
+  {
+    id: 9,
+    name: "Bathrobe",
+    description: "Plush and soft bathrobe.",
+    price: 99.99,
+    category: CATEGORIES.GOODS
+  },
+  {
+    id: 10,
+    name: "Coffee Maker",
+    description: "Coffee maker for fresh brews.",
+    price: 199.99,
+    category: CATEGORIES.GOODS
+  }
 ];
