2a460264bdba7587f351218bccfd1e401d2d74b9
Jayesh <en001507@gmail.com>
2024-08-03 13:38:47 +0530

    Bug: Fixes serialization errors

--- src/redux/invoicesSlice.js
+++ src/redux/invoicesSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { updateBulkProducts, updateProduct } from "./productsSlice";
-import { getUpdatedPricedForInvoice } from "../utils/updateInvoice";
 import { CATEGORIES } from "../constants/categories";
 
 
@@ -28,7 +27,7 @@ const invoicesSlice = createSlice({
         const updatedProduct = action.payload;
         state.forEach((invoice, index) => {
 
-          const productIndex = invoice.items.findIndex((record) => record.itemId == updatedProduct.id);
+          const productIndex = invoice.items.findIndex((record) => record.itemId === updatedProduct.id);
           if (productIndex !== -1) {
 
             const itemInsideInvoice = invoice.items[productIndex];
@@ -52,7 +51,7 @@ const invoicesSlice = createSlice({
             if (updatedProduct.category === CATEGORIES.GOODS) {
               invoice.goodsTotal = parseFloat((parseFloat(invoice.goodsTotal) || 0) + currItemCost - prevItemCost).toFixed(2).toString();
             } else {
-              invoice.serviceTotal = parseFloat((parseFloat(invoice.serviceTotal) || 0) + currItemCost - prevItemCost).toFixed(2).toString;
+              invoice.serviceTotal = parseFloat((parseFloat(invoice.serviceTotal) || 0) + currItemCost - prevItemCost).toFixed(2).toString();
             }
 
             let taxRate = parseFloat(invoice.taxRate) || 0;
@@ -66,9 +65,6 @@ const invoicesSlice = createSlice({
         })
       })
       .addCase(updateBulkProducts, (state, action) => {
-
-        console.log(`bulkUpdayes inside invoice slice : payload}`, action.payload);
-
         if (!action.payload || !Array.isArray(action.payload)) return;
 
         // for easier retrival
@@ -77,29 +73,14 @@ const invoicesSlice = createSlice({
           return accumulator;
         }, {});
 
-
-        console.log(`bulkUpdayes inside invoice slice : mapped Products}`, productIdMapped);
-
-
         state.forEach((invoice) => {
           // traverse their items and see if it matched with updated products
           invoice.items.forEach((item) => {
-
-            console.log(`bulkUpdayes inside invoice slice : item id}`, item.itemId);
-            console.log(`bulkUpdayes inside invoice slice : item}`, item);
             const updatedProduct = productIdMapped[item.itemId];
-            console.log(`bulkUpdayes inside invoice slice : updatedProd}`, updatedProduct);
-            // console.log(`bulkUpdayes inside invoice slice : updatedProd id : }`, );
 
 
             if (updatedProduct) {
               // means we need to  update this product
-
-              console.log(`bulkUpdayes inside invoice slice : item product found}`, updatedProduct);
-              console.log(`bulkUpdayes inside invoice slice : item id}`, item.id);
-              console.log(`bulkUpdayes inside invoice slice : prod id }`, updatedProduct.id);
-
-
               const prevItemCost = parseFloat(item.itemPrice).toFixed(2) * parseInt(item.itemQuantity, 10);
 
 
@@ -114,7 +95,7 @@ const invoicesSlice = createSlice({
               if (updatedProduct.category === CATEGORIES.GOODS) {
                 invoice.goodsTotal = parseFloat((parseFloat(invoice.goodsTotal) || 0) + currItemCost - prevItemCost).toFixed(2).toString();
               } else {
-                invoice.serviceTotal = parseFloat((parseFloat(invoice.serviceTotal) || 0) + currItemCost - prevItemCost).toFixed(2).toString;
+                invoice.serviceTotal = parseFloat((parseFloat(invoice.serviceTotal) || 0) + currItemCost - prevItemCost).toFixed(2).toString();
               }
 
               let taxRate = parseFloat(invoice.taxRate) || 0;
