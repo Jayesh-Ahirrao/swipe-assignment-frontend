201ec2e1c1ff6cd52dc90248ae0243bb96e7cda7
Jayesh <en001507@gmail.com>
2024-08-05 05:19:29 +0530

    Feat: defined slice for exchange as we fetching data from api

--- src/constants/currencies.js
+++ src/constants/currencies.js
@@ -1,14 +1,14 @@
 // using two endpoint as we dont have support for bitcoin and currencies in single api endpoint
 export const API_ENDPOINTS = {
-    FRANKFURTER: 'https://api.frankfurter.app/latest',
-    COINGECKO: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd',
+    CURRENCIES: 'https://api.frankfurter.app/latest',
+    CRYPTO: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd',
 };
 
 
-//as our min value is 0.1  and for this value of bitcoin will be 0.000017 so we need 6 point precision for bitcoin
+//as our min value is 0.1  and for this value of bitcoin will be 0.000017... so lets us 10
 export const DECIMAL_PLACES = {
     CURRENCIES: 2,
-    BITCOIN: 6,
+    BITCOIN: 10,
 };
 
 
